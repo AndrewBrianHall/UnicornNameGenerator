0fc51167863aa78a7a8632bf14728b721f38742e
Andrew Hall <andrew.hall@uipath.com>
2019-10-13 22:59:02 -0700

    gulp min tweaks

--- UnicornNames.Web/gulpfile.js
+++ UnicornNames.Web/gulpfile.js
@@ -8,17 +8,29 @@ var inline = require('gulp-inline');
 var minifyCss = require('gulp-minify-css');
 const uglify = require('gulp-uglify');
 var template = require('gulp-template');
+const htmlmin = require('gulp-htmlmin');
 var fs = require('fs');
 
 var workingDir = "./obj/";
 var outputDir = "./bld/";
 const keyFile = "keys.json";
 
+gulp.task('minhtml', () => {
+    return gulp.src(outputDir + '*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(outputDir));
+});
+
 gulp.task('inline', function() {
     return gulp.src(workingDir + '*.html')
         .pipe(inline({
             base: workingDir,
-            // js: uglify,
+            js: minify({
+                ext: {
+                    min: '.js'
+                },
+                noSource: true
+            }),
             css: [minifyCss]
         }))
         .pipe(gulp.dest(outputDir));
@@ -31,12 +43,6 @@ gulp.task('pack-js', function() {
     return gulp.src(['appinsights.js', 'unicornname.js'])
         .pipe(concat('unicornname.js'))
         .pipe(template(key))
-        .pipe(minify({
-            ext: {
-                min: '.js'
-            },
-            noSource: true
-        }))
         .pipe(gulp.dest(workingDir));
 });
 
@@ -48,4 +54,4 @@ gulp.task('copy', function() {
         .pipe(gulpCopy(workingDir, { prefix: 1 }))
 });
 
-gulp.task('default', gulp.series('pack-js', 'copy', 'inline'));
+gulp.task('default', gulp.series('pack-js', 'copy', 'inline', 'minhtml'));
